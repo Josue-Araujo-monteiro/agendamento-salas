7823521a93c0d44925c2076ef500e6095f1f8249
Leandra <leandra@MacBook-Air-de-Leandra.local>
2024-06-29 18:37:48 -0300

    commit03

--- agendamento-salas/src/app.jsx
+++ agendamento-salas/src/app.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { SupabaseProvider } from './contexts/SupabaseContext';
 import Login from './components/Login';
+import Register from './components/Register';  // Importe o componente Register
 import Dashboard from './components/Dashboard';
 import RoomManagement from './components/RoomManagement';
 import AvailableRooms from './components/AvailableRooms';
@@ -14,6 +15,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route path="/register" component={Register} />  // Adicione esta linha
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/room-management" component={RoomManagement} />
           <Route path="/available-rooms" component={AvailableRooms} />
